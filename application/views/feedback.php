246531e9be33ba792d4f3a001d60245c7b05b3c6
yiguid <yiguid@gmail.com>
2013-03-23 00:28:43 +0800

    反馈留言页面美化第一步

--- application/views/feedback.php
+++ application/views/feedback.php
@@ -10,119 +10,120 @@
 	}
 	$this->load->helper('url');
 ?>
-<div>
-
-
-	<div class="view">
-	<?php
-		$this->load->model('feedback_mdl');
-		$typearr=array('催单','网站错误','功能建议','投诉');
-		foreach($msglist as $row)
-		{
-
-			echo "<div>";
-			echo $row->content;
-			echo '<br/>';
-			echo $typearr[$row->type - 1];
-			echo $row->date;
-			echo $row->time;
-			echo $row->nickname;
-			if($this->auth->logged_in()){
-				echo anchor("feedback/reply/$row->id", "回复");
-			}
-			echo "</div>";
-			$rpylist = $this->feedback_mdl->get_msg_all_rpy($row->id);
-			foreach($rpylist as $rpy)
+<div id="feedback_container">
+	<div id="profile">
+		<div class="feedback_view">
+		<?php
+			$this->load->model('feedback_mdl');
+			$typearr=array('催单','网站错误','功能建议','投诉');
+			foreach($msglist as $row)
 			{
-				echo "<div>";
-				echo $rpy->content;
-				echo $rpy->date;
-				echo $rpy->time;
-				echo $rpy->nickname;
+
+				echo "<div class='feedback_message'>";
+				echo "[".$typearr[$row->type - 1]."]";
+				echo $row->content;
+				echo '<br/>';
+				echo $row->nickname."&nbsp;&nbsp;&nbsp;";
+				echo $row->date." ";
+				echo $row->time." ";
+				if($this->auth->logged_in()){
+					echo anchor("feedback/reply/$row->id", "[回复]");
+				}
 				echo "</div>";
+				$rpylist = $this->feedback_mdl->get_msg_all_rpy($row->id);
+				foreach($rpylist as $rpy)
+				{
+					echo "<div class='feedback_reply'>";
+					echo $rpy->content."<br/>";
+					echo $rpy->nickname."&nbsp;&nbsp;&nbsp;";
+					echo $rpy->date." ";
+					echo $rpy->time;	
+					echo "</div>";
+				}
 			}
-		}
-	?>
+		?>
+		</div>
+		
+		<div class="pagination" id="pagelist">
+			<ul>
+			<?php
+			 $path = base_url().'feedback/display';
+			 $prevPage = max(1,$curPage-1);
+			 $nextPage = min($curPage+1,$maxPage);
+			 $startPage = max(1,$curPage - 3);
+			 $endPage = min($curPage + 3,$maxPage);
+			 if($curPage > 1)
+			 {
+			 	echo '<li>';
+			 	echo anchor("$path/1/$maxPage", '<<');
+			 	echo '</li>';
+			 	echo '<li>';
+			 	echo anchor("$path/$prevPage/$maxPage", '<');
+			 	echo '</li>';
+			 }
+			 for($i = $startPage;$i<=$endPage;$i++)
+			 {
+			 	if($i==$curPage)
+			 	{
+			 		echo '<li class="disabled">';
+			 	}
+			 	else
+			 	{
+			 		echo '<li class="active">';
+			 	}
+			 	echo anchor("$path/$i/$maxPage", "$i");
+			 	echo '</li>';
+			 }
+			 
+			 if($curPage < $maxPage)
+			 {
+			 	echo '<li>';
+			 	echo anchor("$path/$nextPage/$maxPage", '>');
+			 	echo '</li>';
+			 	echo '<li>';
+			 	echo anchor("$path/$maxPage/$maxPage", '>>');
+			 	echo '</li>';
+			 }
+			?>
+			</ul>
+		</div>
+		<?php if($this->auth->logged_in()){	?>
+		<div class="create">
+			<form class="form-horizontal" action="<?php echo base_url();?>feedback/create" method="post">
+			 <div class="control-group">
+			    <span style="padding-top:6px;" class="help-inline">* 留言类型：</span>
+			    <label class="radio inline">
+			      <input class="inline" type="radio" name="msgtype" value="1" checked>
+			      	催单
+			    </label>
+			    <label class="radio inline">
+			      <input type="radio" name="msgtype" value="2">
+			      	网站错误
+			    </label>
+			    <label class="radio inline">
+			      <input type="radio" name="msgtype" value="3">
+			      	功能建议
+			    </label>
+			    <label class="radio inline">
+			      <input type="radio" name="msgtype" value="4">
+					投诉
+			    </label>
+			</div>
+			<div>
+			     <textarea name="msgcontent" id="msgcontent"></textarea>
+			</div>
+			<div style="padding:10px;">
+			    <button class="btn btn-primary" type="submit" name="submit">发表留言</button>
+			    <button class="btn btn-inverse" type="reset" name="reset">重写</button>
+			</div>
+			</form>
+		</div>
+		
+		<?php }elseif(!$this->auth->printer_logged_in()){?>
+		<div>
+			 若您要留言？请您先<?php echo anchor(base_url().'login','登录');?>
+		</div>
+		<?php }?>
 	</div>
-	
-	<div class="pagination" id="pagelist">
-	<ul>
-	<?php
-	 $path = base_url().'feedback/display';
-	 $prevPage = max(1,$curPage-1);
-	 $nextPage = min($curPage+1,$maxPage);
-	 $startPage = max(1,$curPage - 3);
-	 $endPage = min($curPage + 3,$maxPage);
-	 if($curPage > 1)
-	 {
-	 	echo '<li>';
-	 	echo anchor("$path/1/$maxPage", '<<');
-	 	echo '</li>';
-	 	echo '<li>';
-	 	echo anchor("$path/$prevPage/$maxPage", '<');
-	 	echo '</li>';
-	 }
-	 for($i = $startPage;$i<=$endPage;$i++)
-	 {
-	 	if($i==$curPage)
-	 	{
-	 		echo '<li class="disabled">';
-	 	}
-	 	else
-	 	{
-	 		echo '<li class="active">';
-	 	}
-	 	echo anchor("$path/$i/$maxPage", "$i");
-	 	echo '</li>';
-	 }
-	 
-	 if($curPage < $maxPage)
-	 {
-	 	echo '<li>';
-	 	echo anchor("$path/$nextPage/$maxPage", '>');
-	 	echo '</li>';
-	 	echo '<li>';
-	 	echo anchor("$path/$maxPage/$maxPage", '>>');
-	 	echo '</li>';
-	 }
-	?>
-	</ul>
-	</div>
-	<?php if($this->auth->logged_in()){	?>
-	<div class="create">
-		<form class="form-horizontal" action="<?php echo base_url();?>feedback/create" method="post">
-		 <div class="control-group">
-		    <span class="help-inline">* 留言类型：</span>
-		    <label class="radio inline">
-		      <input type="radio" name="msgtype" value="1" checked>
-		      	催单
-		    </label>
-		    <label class="radio inline">
-		      <input type="radio" name="msgtype" value="2">
-		      	网站错误
-		    </label>
-		    <label class="radio inline">
-		      <input type="radio" name="msgtype" value="3">
-		      	功能建议
-		    </label>
-		    <label class="radio inline">
-		      <input type="radio" name="msgtype" value="4">
-				投诉
-		    </label>
-		    </div>
-		    
-		     <textarea name="msgcontent" id="msgcontent"></textarea>
-		     
-		     <p>
-		     <button class="btn btn-primary" type="submit" name="submit">发表留言</button>
-		     </p>
-		</form>
-	</div>
-	
-	<?php }elseif(!$this->auth->printer_logged_in()){?>
-	<div>
-		 若您要留言？请您先<?php echo anchor(base_url().'login','登录');?>
-	</div>
-	<?php }?>
 </div>
 <?php $this->load->view('footer'); ?>
--- application/views/printer/login.php
+++ application/views/printer/login.php
@@ -3,44 +3,46 @@
 $this->load->view('printer/header');
 ?>
 <div id="printer_container">
-	<div id="login_main">
-		<div id="login_left">
-			<h1>Welcome to 7KMall!</h1>
-			<h2>Printer Login!</h2>
-			<div id="myCarousel" class="carousel slide">
-			  <ol class="carousel-indicators">
-			    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-			    <li data-target="#myCarousel" data-slide-to="1"></li>
-			    <li data-target="#myCarousel" data-slide-to="2"></li>
-			  </ol>
-			  <!-- Carousel items -->
-			  <div class="carousel-inner">
-			    <div class="active item"><img src="<?php echo base_url();?>images/bootstrap-mdo-sfmoma-01.jpeg"/></div>
-			    <div class="item"><img src="<?php echo base_url();?>images/bootstrap-mdo-sfmoma-02.jpeg"/></div>
-			    <div class="item"><img src="<?php echo base_url();?>images/bootstrap-mdo-sfmoma-03.jpeg"/></div>
-			  </div>
-			  <!-- Carousel nav -->
-			  <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-			  <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+	<div id="profile">
+		<div id="login_main">
+			<div id="login_left">
+				<h1>Welcome to 7KMall!</h1>
+				<h2>Printer Login!</h2>
+				<div id="myCarousel" class="carousel slide">
+				  <ol class="carousel-indicators">
+				    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				    <li data-target="#myCarousel" data-slide-to="1"></li>
+				    <li data-target="#myCarousel" data-slide-to="2"></li>
+				  </ol>
+				  <!-- Carousel items -->
+				  <div class="carousel-inner">
+				    <div class="active item"><img src="<?php echo base_url();?>images/bootstrap-mdo-sfmoma-01.jpeg"/></div>
+				    <div class="item"><img src="<?php echo base_url();?>images/bootstrap-mdo-sfmoma-02.jpeg"/></div>
+				    <div class="item"><img src="<?php echo base_url();?>images/bootstrap-mdo-sfmoma-03.jpeg"/></div>
+				  </div>
+				  <!-- Carousel nav -->
+				  <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+				  <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+				</div>
 			</div>
-		</div>
-		<div id="login_right">
-		<form action="login" method="post">
-			<table>
-				<tr>
-					<td>用户名：</td>
-					<td><input class="input-block-level" type="text" name="username" id="username"></input></td>
-				</tr>
-				<tr>
-					<td>密码：</td>
-					<td><input class="input-block-level" type="password" name="password" id="password"></input></td>
-				</tr>
-				<tr>
-					<td><input class="btn btn-primary" type="submit" name="submit" value="提交"/></td>
-					<td><input class="btn btn-primary" type="reset" name="reset" value="重置"/></td>
-				</tr>
-			</table>
-		</form> 
+			<div id="login_right">
+			<form action="login" method="post">
+				<table>
+					<tr>
+						<td>用户名：</td>
+						<td><input class="input-block-level" type="text" name="username" id="username"></input></td>
+					</tr>
+					<tr>
+						<td>密码：</td>
+						<td><input class="input-block-level" type="password" name="password" id="password"></input></td>
+					</tr>
+					<tr>
+						<td><input class="btn btn-primary" type="submit" name="submit" value="提交"/></td>
+						<td><input class="btn btn-primary" type="reset" name="reset" value="重置"/></td>
+					</tr>
+				</table>
+			</form> 
+			</div>
 		</div>
 	</div>
 </div>
