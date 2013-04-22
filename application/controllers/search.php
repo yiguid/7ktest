ad6410a2ed2f374d06fd60faa5cfb0d163d19535
Baolong Jia <jiabaolongbuaa@gmail.com>
2013-04-22 23:34:24 +0800

    添加search的controller和view，内容待修改（学习中）

---
+++ application/controllers/search.php
@@ -0,0 +1,59 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Search extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -  
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in 
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+
+	public $data = array();
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->model('user_mdl');
+		$this->data['page_title'] = '注册';
+	}
+
+	public function index()
+	{
+		$this->form_validation->set_rules('reg_username','用户名','required|trim|min_length[5]|max_length[12]');
+		$this->form_validation->set_rules('reg_nickname','昵称','required|trim');
+		$this->form_validation->set_rules('reg_password','密码','required|trim|matches[re_reg_password]');
+		$this->form_validation->set_rules('re_reg_password','重复密码','required|trim');
+		$this->form_validation->set_rules('reg_email','邮箱','required|trim|valid_email');
+		if($this->form_validation->run() == FALSE){
+			$this->data['regist'] = TRUE;
+			$this->load->view('login',$this->data);
+		}else if($this->user_mdl->regist(
+			array(
+				'username' => $this->input->post('reg_username'),
+				'nickname' => $this->input->post('reg_nickname'),
+				'password' => $this->input->post('reg_password'),
+				'email' => $this->input->post('reg_email')
+			)
+		)){
+			$this->data['regist_info'] = '注册成功，请登陆。';
+			$this->load->view('searchresult',$this->data);
+		}else{
+			$this->data['regist'] = TRUE;
+			$this->load->view('searchresult',$this->data);
+		}
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
---
+++ application/views/searchresult.php
@@ -0,0 +1,67 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+$this->load->view('header');
+$this->load->view('menu');
+?>
+		<div id="managebox">
+			<div class="content-header">
+				<h4>搜索结果</h4>
+			</div>
+			<table style="width:700px;" class="table table-hover">
+				<tr>
+					<td>ID</td><td>文件名</td><td>关键词</td><td>类型</td><td>大小</td><td>上传时间</td><td>下载地址</td>
+				</tr>
+				<?php foreach($documenthistorylist as $doc):?>  
+					<tr>
+					<?php echo "<td>".$doc->id ."</td><td>".substr($doc->name, 0,30)."</td><td>".$doc->keyword."</td><td>".$doc->type."</td><td>".$doc->size."</td><td>".$doc->uploadtime."</td><td><a href=\"uploads/".$doc->url."\" >另存为</a></td>";?>
+					</tr>  
+  
+					<?php endforeach;?>
+			</table>
+			<div class="pagination btn" id="pagelist">
+				<ul>
+				<?php
+				 $path = base_url().'documenthistory/display';
+				 $prevPage = max(1,$curPage-1);
+				 $nextPage = min($curPage+1,$maxPage);
+				 $startPage = max(1,$curPage - 3);
+				 $endPage = min($curPage + 3,$maxPage);
+				 if($curPage > 1)
+				 {
+				 	echo '<li>';
+				 	echo anchor("$path/1", '<<');
+				 	echo '</li>';
+				 	echo '<li>';
+				 	echo anchor("$path/$prevPage", '<');
+				 	echo '</li>';
+				 }
+				 for($i = $startPage;$i<=$endPage;$i++)
+				 {
+				 	if($i==$curPage)
+				 	{
+				 		echo '<li class="disabled">';
+				 	}
+				 	else
+				 	{
+				 		echo '<li class="active">';
+				 	}
+				 	echo anchor("$path/$i", "$i");
+				 	echo '</li>';
+				 }
+				 
+				 if($curPage < $maxPage)
+				 {
+				 	echo '<li>';
+				 	echo anchor("$path/$nextPage", '>');
+				 	echo '</li>';
+				 	echo '<li>';
+				 	echo anchor("$path/$maxPage", '>>');
+				 	echo '</li>';
+				 }
+				?>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php $this->load->view('footer'); ?>
