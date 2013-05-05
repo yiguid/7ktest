43c152fa55252cb585746d4aa1f8048c9cc9ae75
yiguid <yiguid@gmail.com>
2013-05-05 21:54:39 +0800

    打印店中心：基本信息，修改密码
    css给printer单独分了一个文件
    打印店中心的实际操作都在printer/admin.php

--- application/views/printer/password.php
+++ application/views/printer/password.php
@@ -6,8 +6,32 @@ $this->load->view('printer/menu');
 			<div class="content-header">
 				<h4>修改密码</h4>
 			</div>
-			<div>
-				待添加
+			<div class="manage_content">
+				<form action="<?php echo base_url();?>printer/admin/password" method="post">
+					<table>
+						<tr>
+							<td>原密码：</td>
+							<td><input style="width:180px;" class="input-block-level" type="password" name="old_password" id="old_password"></input></td>
+							<td><?php echo form_error('old_password')?></td>
+						</tr>
+						<tr>
+							<td>新密码：</td>
+							<td><input style="width:180px;" class="input-block-level" type="password" name="new_password" id="new_password"></input></td>
+							<td><?php echo form_error('new_password')?></td>
+						</tr>
+						<tr>
+							<td>重复新密码：</td>
+							<td><input style="width:180px;" class="input-block-level" type="password" name="re_new_password" id="re_new_password"></input></td>
+							<td><?php echo form_error('re_new_password')?></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><input class="btn btn-primary" type="submit" name="submit" value="提交更改"/>
+							</td>
+							<td></td>
+						</tr>
+					</table>
+				</form> 
 			</div>
 		</div>
 	</div>
