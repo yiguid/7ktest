f5495e9a7ba9f59322b6d009f57f8cd93e8a1f1a
yiguid <yiguid@gmail.com>
2013-05-20 21:10:19 +0800

    printer menu 的bug 修复

--- application/views/printer/menu.php
+++ application/views/printer/menu.php
@@ -1,6 +1,6 @@
 <div id="printer_container">
 	<div id="profile">
-		<div id="printer_list">
+		<div class="manage_list">
 			<div class="profile_title">
 				<i style="margin:6px 0 0 30px;" class='icon-user'></i>
 				<a href="manage">打印店中心</a>
@@ -11,11 +11,9 @@
 			<div class="menu_content"><i class='icon-bell'></i><a href="<?php echo base_url();?>printer/printhistory">自行取印任务</a></div>
 			<div class="menu_content"><i class='icon-briefcase'></i><a href="<?php echo base_url();?>printer/printhistory">校园送印任务</a></div>
 			<div class="menu_content"><i class='icon-gift'></i><a href="<?php echo base_url();?>printer/printhistory">快递送印任务</a></div>
-			<div class="printer_divider"></div>
 			<div class="menu_title">文件管理</div>
 			<div class="menu_content"><i class='icon-file'></i><a href="<?php echo base_url();?>printer/documenthistory">印单文件</a></div>
 			<div class="menu_content"><i class='icon-star'></i><a href="<?php echo base_url();?>printer/specialdoc">特色资料</a></div>
-			<div class="printer_divider"></div>
 			<div class="menu_title">打印店管理
 				<a href="<?php echo base_url().'shop/doc/'.$this->session->userdata('id').'-1-1';?>">[查看店铺]</a></div>
 			<div class="menu_content"><i class='icon-eye-open'></i><a href="<?php echo base_url();?>printer/manage/info">基本信息</a></div>
