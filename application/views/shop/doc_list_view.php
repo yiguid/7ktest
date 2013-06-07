e5997a7525257736123a96e8cff3a6f91a8f77e1
LuoJianhui <luojianhuiyali@163.com>
2013-06-07 23:03:58 +0800

    pter主页修改

---
+++ application/views/shop/doc_list_view.php
@@ -0,0 +1,20 @@
+					<table class="table table-hover manage_table">
+						<tr class="table_header">
+							<td>ID</td><td>文件名</td><td>大小</td><td>描述</td><td>页数</td><td>价格</td><td>操作</td>
+						</tr>
+						<?php foreach($docList as $doc):?>  
+							<tr>
+							<?php echo "<td>".$doc->id ."</td><td>".substr($doc->name, 0,30)."</td><td>"
+							.$doc->size."</td><td>".$doc->description."</td><td>".$doc->page."</td><td>"
+							.$doc->price."</td><td>";
+							if($this->session->userdata('user_type') == 'user'){
+								echo "<a href=\"javascript:addSpecDocToPrinttask('".base_url()
+								."','".$doc->id."','".$pterid."','".$name."')\" >印一份</a>";
+							}else{
+								echo "查看";
+							}
+							
+							echo "</td>";?>
+							</tr>
+							<?php endforeach;?>
+					</table>
