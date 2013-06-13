dd7c42ab0a0c8875b3e7d55066f3235c1e7c2c1f
yiguid <yiguid@gmail.com>
2013-06-13 22:11:04 +0800

    fix conflict

--- application/views/search/searchdocresult_list.php
+++ application/views/search/searchdocresult_list.php
@@ -9,7 +9,7 @@
 							."</td><td>".$searchresult->type 
 							."</td><td>".$searchresult->size."</td><td>".
 							"<a href=\"javascript:addSpecDocToPrinttask('".base_url()
-								."','".$searchresult->id."','".$searchresult->uploadpterid."','".""."')\" >印一份</a>"
+								."','".$searchresult->id."','".$searchresult->uploadpterid."','".$searchresult->uploadptername."')\" >印一份</a>"
 							."</td>";?>
 					</tr>  
   
